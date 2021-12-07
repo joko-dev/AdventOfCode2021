2200e5fe64c82d02c290e8dc38aaa0aeb7264a36
joko-dev <61630682+joko-dev@users.noreply.github.com>
2021-12-07 14:15:36 +0100

    Day1 (#1)
    * Day1
    
    - init
    
    * Day 1 - First Part
    
    * Day 1 - Second Part
    
    * Day 1 - Unified

---
+++ AdventOfCode2021/Day1/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using AdventOfCode2021.SharedKernel;
+
+namespace AdventOfCode2021.Day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Depth measurement file:");
+            PuzzleInput puzzleInput = new PuzzleInput(Console.ReadLine());
+
+            List<int> depths = puzzleInput.Lines.ConvertAll(int.Parse);
+
+
+            Console.WriteLine("Larger measurements: {0}", largerMeasurements(depths));
+            Console.WriteLine("Larger measurements one-sliding-window: {0}", largerMeasurementsSliding(depths,1));
+            Console.WriteLine("Larger measurements three-sliding-window: {0}", largerMeasurementsSliding(depths, 3));
+        }
+
+        private static int largerMeasurements(List<int> depths)
+        {
+            int inc = 0;
+
+            for (int i = 0; i < depths.Count; i++)
+            {
+                if ((i > 0) && (depths[i] > depths[i - 1]))
+                {
+                    inc++;
+                }
+            }
+
+            return inc;
+        }
+
+        private static int largerMeasurementsSliding(List<int> depths, int windowSize)
+        {
+            int inc = 0;
+
+            for (int i = windowSize; i < depths.Count; i++)
+            {
+                int current = getSumWindow(depths, i, windowSize);
+                int previous = getSumWindow(depths, i - 1, windowSize);
+                if (current > previous)
+                {
+                    inc++;
+                }
+            }
+
+            return inc;
+        }
+
+        private static int getSumWindow(List<int> depths, int upperIndex, int windowSize)
+        {
+            int sum = 0;
+            for(int i = 0; i < windowSize; i++)
+            {
+                sum = sum + depths[upperIndex - i];
+            }
+
+            return sum;
+        }
+    }
+}
