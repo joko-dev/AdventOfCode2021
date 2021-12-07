ef46213d7421fa0d1b961e064edfbad7e2e1fcd0
joko-dev <jokodev@outlook.de>
2021-12-07 15:08:36 +0100

    Day 2 - Init

---
+++ AdventOfCode2021/Day2/Program.cs
@@ -0,0 +1,32 @@
+﻿using AdventOfCode2021.SharedKernel;
+using System;
+using System.Collections.Generic;
+
+namespace AdventOfCode2021.Day2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Course file:");
+            PuzzleInput puzzleInput = new PuzzleInput(Console.ReadLine());
+
+            List<(string direction, int distance)> course = getCourse(puzzleInput.Lines);
+
+            Console.WriteLine("Larger measurements: {0}");
+        }
+
+        static List<(string direction, int distance)> getCourse (List<string> input)
+        {
+            List<(string direction, int distance)> course = new List<(string, int)>();
+
+            foreach(string move in input)
+            {
+                string[] temp = move.Split(' ');
+                course.Add((temp[0], int.Parse(temp[1])));
+            }
+
+            return course;
+        }
+    }
+}
