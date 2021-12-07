35c33f77bf3cfc52a828f2d2b6fc653d98122b58
joko-dev <jokodev@outlook.de>
2021-12-07 13:03:41 +0100

    Day1
    - init

---
+++ AdventOfCode2021/Day1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AdventOfCode2021.Day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ AdventOfCode2021/SharedKernel/PuzzleInput.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace AdventOfCode2021.SharedKernel
+{
+    public class PuzzleInput
+    {
+
+    }
+}
