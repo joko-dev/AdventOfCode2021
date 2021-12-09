e67ac44297988048c9f0e690a6732405925ddcde
joko-dev <jokodev@outlook.de>
2021-12-09 17:11:10 +0100

    Day8 - First part

---
+++ AdventOfCode2021/Day8/DisplayDigit.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2021.Day8
+{
+    class DisplayDigit
+    {
+        public static readonly int[] uniqueDigits = {1,4,7,8};
+
+        private List<char> segments;
+
+        public DisplayDigit(string segments)
+        {
+            this.segments = new List<char>();
+            foreach(char segment in segments)
+            {
+                this.segments.Add(segment);
+            }
+        }
+
+        public int? getDigit()
+        {
+            int? digit = null;
+
+            switch (segments.Count())
+            {
+                case 2: 
+                    digit = 1;
+                    break;
+                case 4:
+                    digit = 4;
+                    break;
+                case 3:
+                    digit = 7;
+                    break;
+                case 7:
+                    digit = 8;
+                    break;
+            }
+
+            return digit;
+        }
+    }
+}
---
+++ AdventOfCode2021/Day8/Program.cs
@@ -0,0 +1,52 @@
+﻿using AdventOfCode2021.SharedKernel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode2021.Day8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Signal Pattern Notes: ");
+            PuzzleInput puzzleInput = new PuzzleInput(Console.ReadLine(), true);
+
+            List<DisplayDigit> outputDigits = getOutputDigits(puzzleInput.Lines);
+            int countUniqueNumberSequences = countNumbersWithUniqueSequenceCount(outputDigits);
+
+            Console.WriteLine("Appeareance 1,4,7,8 in output: {0}", countUniqueNumberSequences);
+        }
+
+        private static List<DisplayDigit> getOutputDigits(List<string> lines)
+        {
+            List<DisplayDigit> outputDigits = new List<DisplayDigit>();
+            foreach (string line in lines)
+            {
+                string output = line.Split('|')[1];
+                string[] digits = output.Split(' ');
+                foreach(string digit in digits)
+                {
+                    outputDigits.Add(new DisplayDigit(digit));
+                }
+            }
+
+            return outputDigits;
+        }
+
+        private static int countNumbersWithUniqueSequenceCount(List<DisplayDigit> outputDigits)
+        {
+            int count = 0;
+
+            foreach (DisplayDigit digit in outputDigits)
+            {
+                if(digit.getDigit().HasValue && DisplayDigit.uniqueDigits.Contains((int)digit.getDigit()))
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
