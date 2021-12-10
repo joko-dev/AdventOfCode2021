32d565f63b76f8ea216193208559c614d0be5040
joko-dev <jokodev@outlook.de>
2021-12-10 21:46:57 +0100

    Day10 - Second part

--- AdventOfCode2021/Day10/Program.cs
+++ AdventOfCode2021/Day10/Program.cs
@@ -7,7 +7,8 @@ namespace AdventOfCode2021.Day10
 {
     class Program
     {
-        static List<(char open, char close, int score)> bracketDefinition = new List<(char, char, int)> { ('(', ')', 3), ('[', ']', 57), ('{', '}', 1197), ('<', '>', 25137) };
+        static List<(char open, char close, int corruptedScore, int incompleteScore)> bracketDefinition 
+                        = new List<(char, char, int, int)> { ('(', ')', 3, 1), ('[', ']', 57, 2), ('{', '}', 1197, 3), ('<', '>', 25137, 4) };
         static void Main(string[] args)
         {
             Console.WriteLine(PuzzleOutputFormatter.getPuzzleCaption("Day 10: Syntax Scoring"));
@@ -16,6 +17,11 @@ namespace AdventOfCode2021.Day10
 
             int sumSyntaxErrorScore = getSyntaxErrorScores(puzzleInput.Lines).Sum();
             Console.WriteLine("Sum syntax error scxore: {0}", sumSyntaxErrorScore);
+
+            List<Int64> incompletedScores = getIncompletedScores(puzzleInput.Lines);
+            //allways odd, division 2 and following upround will give the correct index
+            Int64 middleScoreIncompleted = incompletedScores.OrderBy(s => s).ToList()[(int)incompletedScores.Count/2];
+            Console.WriteLine("Middle score of incompleted lines: {0}", middleScoreIncompleted);
         }
 
         private static List<int> getSyntaxErrorScores(List<string> lines)
@@ -28,6 +34,50 @@ namespace AdventOfCode2021.Day10
             }
 
             return scores;
+        }
+
+        private static List<Int64> getIncompletedScores(List<string> lines)
+        {
+            List<Int64> scores = new List<Int64>();
+
+            foreach (string line in lines)
+            {
+                if (isLineIncompleted(line))
+                {
+                    scores.Add(getInvalidScore(line));
+                }
+            }
+
+            return scores;
+        }
+
+        private static bool isLineIncompleted(string line)
+        {
+            List<char> openedChunks = getChunkList(line);
+
+            return (openedChunks.Count > 0) & (getSyntaxErrorScore(line) == 0);
+        }
+
+        private static List<char> getChunkList(string line, out int lastIndex)
+        {
+            List<char> openedChunks = new List<char>();
+            lastIndex = -1;
+
+            foreach (char bracket in line)
+            {
+                lastIndex++;
+                if (isOpeningBracket(bracket))
+                {
+                    openedChunks.Add(bracket);
+                }
+                else if (isValidClosingBracket(openedChunks[openedChunks.Count - 1], bracket))
+                {
+                    openedChunks.RemoveAt(openedChunks.Count - 1);
+                }
+                
+            }
+
+            return openedChunks;
         }
 
         private static int getSyntaxErrorScore(string line)
@@ -47,9 +97,23 @@ namespace AdventOfCode2021.Day10
                 }
                 else
                 {
-                    score = getScoreForBracket(bracket);
+                    score = getCurruptedScoreForBracket(bracket);
                     break;
                 }
+            }
+
+            return score;
+        }
+        private static Int64 getInvalidScore(string line)
+        {
+            Int64 score = 0;
+            List<char> openedChunks = getChunkList(line);
+            openedChunks.Reverse();
+
+            foreach (char chunk in openedChunks)
+            {
+                score *= 5;
+                score += getIncompleteScoreForBracket(getClosingBracket(chunk));
             }
 
             return score;
@@ -62,12 +126,22 @@ namespace AdventOfCode2021.Day10
 
         private static bool isValidClosingBracket(char openingBracket, char closingBracket)
         {
-            return bracketDefinition.Where(b => b.open == openingBracket).First().close == closingBracket;
+            return getClosingBracket(openingBracket) == closingBracket;
         }
 
-        private static int getScoreForBracket(char bracket)
+        private static char getClosingBracket(char openingBracket)
         {
-            return bracketDefinition.Where(b => b.close == bracket).First().score;
+            return bracketDefinition.Where(b => b.open == openingBracket).First().close;
+        }
+
+        private static int getCurruptedScoreForBracket(char bracket)
+        {
+            return bracketDefinition.Where(b => b.close == bracket).First().corruptedScore;
+        }
+
+        private static int getIncompleteScoreForBracket(char bracket)
+        {
+            return bracketDefinition.Where(b => b.close == bracket).First().incompleteScore;
         }
     }
 }
