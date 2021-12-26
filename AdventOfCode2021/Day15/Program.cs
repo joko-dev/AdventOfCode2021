ebbc9807e5eb3ef694488dc287660ad960728ed8
joko-dev <jokodev@outlook.de>
2021-12-26 19:52:32 +0100

    backup

--- AdventOfCode2021/Day15/Program.cs
+++ AdventOfCode2021/Day15/Program.cs
@@ -15,116 +15,34 @@ namespace AdventOfCode2021.Day15
 
             int[,] cave = PuzzleConverter.getInputAsMatrixInt(puzzleInput.Lines);
 
-            int lowestRisk = findLowestTotalRisk(cave);
-            Console.WriteLine("lowest total risk: {0}", lowestRisk);
+            //Pathfinding algorithm - Dijkstra - https://de.wikipedia.org/wiki/Dijkstra-Algorithmus
+            List<Coordinate> riskList = buildRiskList(cave);
+            Dictionary<Coordinate,Coordinate> predecessors = Dijkstra(riskList, riskList.Find(r => r.X == 0 && r.Y == 0));
+            List<Coordinate> path = getShortestPath(riskList.Find(r => r.X == cave.GetLength(0) - 1 && r.Y == cave.GetLength(1) - 1), predecessors);
+            Console.WriteLine("Lowest risk top left to bottom right: {0}", getRiskForPath(cave, path));
         }
 
-        private static int findLowestTotalRisk(int[,] cave)
+        private static List<Coordinate> buildRiskList(int[,] cave)
         {
-            //determine starting risk for shortest path
-            List<Coordinate> firstPath = getFirstPath(cave);
-            int lowestRisk = getRiskForPath(cave, firstPath);
+            List<Coordinate> riskList = new List<Coordinate>();
 
-            List<List<Coordinate>> paths = getPathsForCave(cave, null, new Coordinate(0, 0), ref lowestRisk) ;
-
-            paths = removeUnfinishedPaths(cave, paths);
-            lowestRisk = getLowestRisk(cave, paths);
-
-            return lowestRisk;
-        }
-
-        private static List<List<Coordinate>> removeUnfinishedPaths(int[,] cave, List<List<Coordinate>> paths)
-        {
-            List<List<Coordinate>> fixedPaths = new List<List<Coordinate>>();
-
-            foreach(List<Coordinate> path in paths)
+            for (int x = 0; x < cave.GetLength(0); x++)
             {
-                if(isEndingPoint(cave, path.Last()))
+                for (int y = 0; y < cave.GetLength(1); y++)
                 {
-                    fixedPaths.Add(path);
+                    Coordinate coordinate = new Coordinate(x, y);
+                    riskList.Add(coordinate);
                 }
             }
 
-            return fixedPaths;
-        }
-
-        private static List<List<Coordinate>> getPathsForCave(int[,] cave, List<Coordinate> currentPath, Coordinate point, ref int lowestRisk)
-        {
-            List<List<Coordinate>> newPaths = new List<List<Coordinate>>();
-            bool firstPath = true;
-
-            if (currentPath == null)
-            {
-                currentPath = new List<Coordinate>();
-                newPaths.Add(currentPath);
-            }
-            currentPath.Add(point);
-
-            int currentPathRisk = getRiskForPath(cave, currentPath);
-            List<Coordinate> currentPathOriginal = new List<Coordinate>(currentPath);
-            
-            if (isEndingPoint(cave, point))
-            {
-                if(currentPathRisk < lowestRisk)
-                {
-                    lowestRisk = currentPathRisk;
-                }
-            }
-            else if (currentPathRisk < lowestRisk)
-            {
-                List<Coordinate> adjacent = PuzzleConverter.getAdjacentPoints(cave, ((int x, int y)) point, true, true, false).Select(x => new Coordinate(x)).ToList();
-
-                foreach(Coordinate adj in adjacent)
-                {
-                    if (!currentPath.Exists(x => x.X == adj.X && x.Y == adj.Y))
-                    {
-                        List<Coordinate> path = null;
-                        if (firstPath)
-                        {
-                            path = currentPath;
-                        }
-                        else
-                        {
-                            path = new List<Coordinate>(currentPathOriginal);
-                            newPaths.Add(path);
-                        }
-
-                        newPaths.AddRange(getPathsForCave(cave, path, adj, ref lowestRisk));
-
-                        firstPath = false;
-                    }
-                }
-            }
-
-            return newPaths;
-        }
-
-        private static bool isEndingPoint(int[,] cave, Coordinate point)
-        {
-            return (point.X == cave.GetLength(0) - 1) && (point.Y == cave.GetLength(1) - 1);
-        }
-
-        private static int getLowestRisk(int[,] cave, List<List<Coordinate>> paths)
-        {
-            int lowestRisk = int.MaxValue;
-
-            foreach(List<Coordinate> path in paths)
-            {
-                int risk = getRiskForPath(cave, path);
-                if(risk < lowestRisk)
-                {
-                    lowestRisk = risk;
-                }
-            }
-
-            return lowestRisk;
+            return riskList;
         }
 
         private static int getRiskForPath(int[,] cave, List<Coordinate> path)
         {
             int risk = 0;
-            
-            for(int i = 1; i < path.Count(); i++) 
+
+            for(int i = 1; i < path.Count; i++)
             {
                 risk += cave[path[i].X, path[i].Y];
             }
@@ -132,21 +50,105 @@ namespace AdventOfCode2021.Day15
             return risk;
         }
 
-        private static List<Coordinate> getFirstPath(int[,] cave)
+        private static List<Coordinate> getShortestPath(Coordinate endPoint, Dictionary<Coordinate, Coordinate> predecessors)
         {
             List<Coordinate> path = new List<Coordinate>();
-
-            for (int h = 0; h < cave.GetLength(1); h++)
+            path.Add(endPoint);
+            Coordinate u = endPoint;
+            while (predecessors[u] is not null)
             {
-                path.Add(new Coordinate(0, h));
+                u = predecessors[u];
+                path.Insert(0, u);
             }
-
-            for (int w = 1; w < cave.GetLength(0); w++)
-            {
-                path.Add(new Coordinate(w, cave.GetLength(1) - 1));
-            }
-
             return path;
         }
+
+        static Dictionary<Coordinate, Coordinate> Dijkstra(int[,] cave, Coordinate start)
+        {
+            Dictionary<Coordinate, int> distance = new Dictionary<Coordinate, int>();
+            Dictionary<Coordinate, Coordinate> predecessor = new Dictionary<Coordinate, Coordinate>(); 
+            List<Coordinate> coordinatesWithoutPath = new List<Coordinate>();
+
+            initialize(cave, start, distance, predecessor, coordinatesWithoutPath);
+            while (coordinatesWithoutPath.Count() > 0)
+            {
+                Coordinate lowestDistance = findCoordinateWithLowestDinstance(coordinatesWithoutPath, distance);
+                coordinatesWithoutPath.Remove(lowestDistance);
+                List<Coordinate> adjacent = getAdjacentCoordinates(cave, lowestDistance, coordinatesWithoutPath);
+                foreach(Coordinate adj in adjacent)
+                {
+                    if (coordinatesWithoutPath.Exists(c => c == adj))
+                    {
+                        distanceUpdate(cave, lowestDistance, adj, distance, predecessor);
+                    }
+                }
+            }
+            return predecessor;
+        }
+
+        private static void distanceUpdate(int[,] cave, Coordinate from, Coordinate to, Dictionary<Coordinate, int> distance, Dictionary<Coordinate, Coordinate> predecessor)
+        {
+            int alternative = distance[from] + cave[from.X, from.Y];
+
+            if (alternative < distance[to])
+            {
+                distance[to] = alternative;
+                predecessor[to] = from;
+            }
+        }
+
+        private static List<Coordinate> getAdjacentCoordinates(int[,] cave, Coordinate coordinate, List<Coordinate> coordinates)
+        {
+            List<Coordinate> adjacent = new List<Coordinate>();
+            List<(int,int)> points = PuzzleConverter.getAdjacentPoints(cave, (coordinate.X, coordinate.Y), true, true, false);
+
+            foreach((int x, int y) point in points)
+            {
+                Coordinate adj = coordinates.Find(c => c.X == point.x && c.Y == point.y);
+                if(adj is not null)
+                {
+                    adjacent.Add(adj);
+                }
+            }
+
+            return adjacent;
+        }
+
+        private static Coordinate findCoordinateWithLowestDinstance(List<Coordinate> coordinatesWithoutPath, Dictionary<Coordinate, int> distance)
+        {
+            Coordinate coordinateLowestDistance = null;
+            distance.OrderBy(d => d.Value);
+            foreach(KeyValuePair<Coordinate, int> coordinate in distance)
+            {
+                if(coordinatesWithoutPath.Exists(c => c == coordinate.Key))
+                {
+                    coordinateLowestDistance = coordinate.Key;
+                    break;
+                }
+                                    
+            }
+
+            return coordinateLowestDistance;
+        }
+
+        static void initialize(int[,] cave, Coordinate start, Dictionary<Coordinate, int> distance, Dictionary<Coordinate,Coordinate> predecessor, List<Coordinate> coordinatesWithoutPath)
+        {
+            distance.Clear();
+            predecessor.Clear();
+            coordinatesWithoutPath.Clear();
+
+            for(int x = 0; x < cave.GetLength(0); x++)
+            {
+                for (int y = 0; y < cave.GetLength(1); y++)
+                {
+                    Coordinate coordinate = new Coordinate(x, y);
+                    distance.Add(coordinate, -1);
+                    predecessor.Add(coordinate,null);
+                    coordinatesWithoutPath.Add(coordinate);
+                }
+            }
+            distance[start] = 0;
+        }
+
     }
 }
